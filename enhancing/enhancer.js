45b37c91cbedc8cda10e105e943ccf2fe99ed833
DaytonS3 <Daytonrsteinmeyer@gmail.com>
2019-05-13 16:51:59 -0400

    added succeed / fail / repair logic

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -2,18 +2,37 @@ module.exports = {
   succeed,
   fail,
   repair,
-  get,
+  get
+};
+
+let item = {
+  name: "item",
+  enhancement: 0,
+  durability: 100
 };
 
 function succeed(item) {
+  if (item.enhancement === 20) {
+    item.enhancement = 20;
+  } else {
+    item.enhancement + 1;
+  }
   return { ...item };
 }
 
 function fail(item) {
+  if (item.enhancement < 15) {
+    item.enhancement - 5;
+  } else if (item.enhancement >= 15) {
+    item.enhancement - 10;
+  } else if (item.enhancement > 16) {
+    item.enhancement - 1;
+  }
   return { ...item };
 }
 
 function repair(item) {
+  item.durability = 100;
   return { ...item };
 }
 
